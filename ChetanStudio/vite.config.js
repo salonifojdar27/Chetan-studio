04693c6144b760fef5a8b52effab0f598bc89c24
salonifojdar27 <salonifojdar27@gmail.com>
2025-11-24 14:29:52 +0530

    completed

--- ChetanStudio/vite.config.js
+++ ChetanStudio/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),], server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
